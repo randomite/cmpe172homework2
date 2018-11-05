3624e4382cbabb28c5742ea5aa637cbc637ea842
Ravin Sardal <ravin.sardal@gmail.com>
2018-11-04 19:20:23 -0800

    Ad home controller

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -1,15 +1,49 @@
 package hello;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import twitter4j.IDs;
+import twitter4j.JSONArray;
+import twitter4j.JSONObject;
+import twitter4j.Location;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Trend;
+import twitter4j.Trends;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterResponse;
+import twitter4j.conf.ConfigurationBuilder;
+
 @Controller
 public class HomeController {
-	 @GetMapping("/home")
-	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) { 
-		 	name = "joooooooo2";
-	    	model.addAttribute("name", name);
-	        return "home";
-	    }
+
+    @GetMapping("/home")
+    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) throws TwitterException {
+    	ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey("BUo2i2DGfvgre5ZG7amnQJwuW")
+    	  .setOAuthConsumerSecret("CKR3aVJlKNFWr8BNKayy16Ir4NkfEUzqntvDKlcpfTAOAnbKTi")
+    	  .setOAuthAccessToken("625448859-W8bxEXgl9j4EBdYZXCvrnAUWI4VTO4OwIeJjugHf")
+    	  .setOAuthAccessTokenSecret("zLWg68fUFQHI1OVKofya8Q3vR9NoxG0r14uhTs7OmEQL6");
+    	TwitterFactory tf = new TwitterFactory(cb.build());
+    	Twitter twitter = tf.getInstance();
+    	
+    	ResponseList<Status> responses = twitter.getHomeTimeline();
+    	JSONArray array = new JSONArray();
+    	for(Status response :responses) {
+    		JSONObject data = new JSONObject();
+			data.put("username", response.getUser().getName());
+			data.put("tweet", response.getText());
+			array.put(data);
+    	}
+        model.addAttribute("name", array.toString());
+        System.out.println(array.toString());
+        return "home";
+    }
+
 }
