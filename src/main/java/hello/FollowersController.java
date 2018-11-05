ebceb73752973c00869db687761ccb2d38c70587
Ravin Sardal <ravin.sardal@gmail.com>
2018-11-04 19:39:53 -0800

    Followers updates with names

--- src/main/java/hello/FollowersController.java
+++ src/main/java/hello/FollowersController.java
@@ -6,17 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import twitter4j.IDs;
+import twitter4j.JSONArray;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
 
 @Controller
 public class FollowersController {
 
     @GetMapping("/followers")
-    public String followers(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String followers(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) throws TwitterException {
     	ConfigurationBuilder cb = new ConfigurationBuilder();
     	cb.setDebugEnabled(true)
     	  .setOAuthConsumerKey("BUo2i2DGfvgre5ZG7amnQJwuW")
@@ -24,14 +27,15 @@ public class FollowersController {
     	  .setOAuthAccessToken("625448859-W8bxEXgl9j4EBdYZXCvrnAUWI4VTO4OwIeJjugHf")
     	  .setOAuthAccessTokenSecret("zLWg68fUFQHI1OVKofya8Q3vR9NoxG0r14uhTs7OmEQL6");
     	TwitterFactory tf = new TwitterFactory(cb.build());
-    	Twitter twitter = tf.getInstance();
-    	try {
-            IDs ids  = twitter.getFollowersIDs(-1);
-            model.addAttribute("name", ids.getIDs().length);
-        } catch (TwitterException te) {
-            te.printStackTrace();
+    	Twitter twitter = tf.getInstance();    	
+    	long cursor = -1;
+        IDs ids = twitter.getFollowersIDs(cursor);
+        JSONArray array = new JSONArray();
+        ResponseList<User> users =  twitter.lookupUsers(ids.getIDs());
+        for(User user: users) {
+        	array.put(user.getName());
         }
-    	
+        model.addAttribute("name", array.toString());
         return "home";
     }
 
